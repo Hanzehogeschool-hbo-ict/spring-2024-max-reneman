be3707c971ee57986717dae63c0eca5b3550ad76
Dn-Dave <davepleijendal@gmail.com>
2024-05-24 11:03:50 +0200

    Update PlayControllerTest.php

--- tests/UnitTest/PlayControllerTest.php
+++ tests/UnitTest/PlayControllerTest.php
@@ -9,27 +9,32 @@ use PHPUnit\Framework\TestCase;
 
 class PlayControllerTest extends TestCase
 {
+    private Session $session;
+    private PlayController $playController;
+
+    protected function setUp(): void {
+        $this->session = new Session();
+        $this->playController = new PlayController($this->session);
+    }
+
     public function testHandlePost()
     {
         // Arrange
-        $session = Session::inst();
         $game = new Game();
         $game->player = 0; // White player
         $game->hand[$game->player] = ['Q' => 1, 'B' => 3]; // Hand with 1 queen bee and 3 other pieces
-        $session->set('game', $game);
-
-        $playController = new PlayController();
+        $this->session->set('game', $game);
 
         // Act
         // White player places three non-queen pieces
-        $playController->handlePost('B', '0,0');
-        $playController->handlePost('B', '0,1');
-        $playController->handlePost('B', '0,2');
+        $this->playController->handlePost('B', '0,0');
+        $this->playController->handlePost('B', '0,1');
+        $this->playController->handlePost('B', '0,2');
 
         // White player tries to place a fourth non-queen piece
-        $playController->handlePost('B', '0,3');
+        $this->playController->handlePost('B', '0,3');
 
         // Assert
-        $this->assertEquals('Must play queen bee', $session->get('error'));
+        $this->assertEquals('Must play queen bee', $this->session->get('error'));
     }
 }
