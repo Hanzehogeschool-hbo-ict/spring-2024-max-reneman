99b44ca045f5573b79a0eb3fc44c0574797b18e8
Max Reneman <maxwf.reneman@gmail.com>
2024-06-23 16:07:32 +0200

    Added test for ant rule A

--- tests/tiles/AntTest.php
+++ tests/tiles/AntTest.php
@@ -23,6 +23,13 @@ class AntTest extends TestCase
     }
 
     public function testIsValidMoveRuleA(){
+        $this->game->board['0,0'] = 'A';
+        $this->game->board['1,0'] = 'A';
+        $this->game->board['1,1'] = 'A';
+        $this->game->board['1,2'] = 'A';
+        $this->assertTrue($this->ant->isValidMove('0,0', '0,1', $this->game));
+        $this->assertTrue($this->ant->isValidMove('0,0', '0,2', $this->game));
+        $this->assertTrue($this->ant->isValidMove('0,0', '0,3', $this->game));
 
     }
     public function testIsValidMoveRuleBD(){
